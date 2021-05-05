d8aad34cf234193157f0a5e43da5ff01f606e6e9
AndronikosGiachanatzis <a.giachanatzis@gmail.com>
2021-05-05 23:16:27 +0300

    add correlation heatmap cmd arguments

--- star_classifier.py
+++ star_classifier.py
@@ -1,5 +1,16 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import argparse
 
+
+def getCMDArgs():
+    parser = argparse.ArgumentParser(description="Trains a neural network to classify different star types",
+                                     usage="python star_classifier [OPTION]...")
+    parser.add_argument("-v", help="Verbosity")
+
+    args = parser.parse_args()
+    return args.file
 
 
 def loadDataset(name):
@@ -15,11 +26,28 @@ def loadDataset(name):
 
     return dataset
 
+
+def heatMap(dataset):
+    '''
+    Generates a correlation heatmap for the dataset's variables
+    :param dataset: The variables of which the correlations will be found
+    '''
+    corr_matrix = dataset.corr()
+    # create the plot and save it
+    plt.figure(figsize=(13,13))
+    heatmap = sns.heatmap(corr_matrix, annot=True, cmap="RdYlGn")
+
+    # save plot
+    plt.savefig('corrs.png')
+    # plt.show()
+
+
 def main():
     # load dataset
     dataset = loadDataset("dataset/star_dataset.csv")
     print(dataset)
     print()
+
     # ----- DATA EXPLORATION -----
     print("GENERAL INFORMATION")
     print(dataset.info())
@@ -34,6 +62,17 @@ def main():
         print("The dataset has some missing values")
     else:
         print("The dataset does not have missing values")
+
+    # check data balance
+    print(dataset["Star_type"].value_counts())    # data seems to be balanced as there are exactly 40 observations of each class
+    print()
+
+    # calculate the correlations between the numerical data
+    print("PEARSON CORRELATION COEFFICIENTS")
+    heatMap(dataset)
+
+
+
 
 if __name__ == "__main__":
     try:
